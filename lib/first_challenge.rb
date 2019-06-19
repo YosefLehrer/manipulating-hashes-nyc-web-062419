67f584f4014c61c4d4e6251056be89451ca66f01
Yosef Lehrer <yolehrer@gmail.com>
2019-06-19 13:15:50 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -18,8 +18,8 @@ contacts.each do |person, data|
   data.each do |attribute, value|
     if attribute == :favorite_icecream_flavors
       value.each do |flavor|
-        flavor.delete_if
-        flavor == "strawberry"
+        flavor.delete_if {flavor == "strawberry"
+        
       end
     end
   end
